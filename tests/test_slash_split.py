efe9efa2798372f1cf7f8bd9bc59275648073eb5
lynma01 <lynma01@gmail.com>
2023-04-04 18:30:31 -0400

    setup pytest in project.toml

--- tests/test_slash_split.py
+++ tests/test_slash_split.py
@@ -3,11 +3,6 @@ from src.utils.parsing import slash_split
 def test_slash_split():
 
     period = {'period': '2021-01-38/2020-02-26'}
-    expect = {
-        'period': {
-              'min': '2020-02-26'
-            , 'max': '2021-01-38'
-        }
-    }
+    expect = {'min': '2020-02-26', 'max': '2021-01-38'}
 
     assert slash_split(period) == expect
